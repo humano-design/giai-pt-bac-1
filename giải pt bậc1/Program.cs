feb4c9c5de54d6e644f1cd4b62a37e32021dfeb4
SPC <SPC@28-LAB402>
2025-02-14 10:28:48 +0700

    Add project files.

---
+++ giải pt bậc1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        Console.WriteLine("Nhập vào a: ");
+        float a, b, x;
+         a = float.Parse(Console.ReadLine());
+
+        if (a == 0)
+        {
+            Console.WriteLine("");
+        }
+        else
+        {
+            Console.WriteLine("Nhập vào b: ");
+             b = float.Parse(Console.ReadLine());
+
+             x = -b / a;
+            Console.WriteLine("{0}x + {1} = 0 \n=> x = {2}", a, b, x);
+        }
+    }
+}
